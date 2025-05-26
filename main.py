12e351d5e90950d5bdffb848b3e8e7ee8849384e
ploki <82970720+ploki365@users.noreply.github.com>
2025-05-26 16:09:02 -0400

    add character counting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 
 def get_book_text(path_to_file):
@@ -8,5 +8,8 @@ def get_book_text(path_to_file):
 def main():
     num_words = count_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+
+    num_char = count_characters(get_book_text("books/frankenstein.txt"))
+    print(num_char)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    char_count = {}
+
+    for char in text.lower():
+        if char not in char_count:
+            char_count[char] = 0
+        char_count[char] += 1
+    
+    return char_count
